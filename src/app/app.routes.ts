9e4667363a20335c55f27c76d5175670198f52b8
tarista13 <tyarista13@gmail.com>
2024-11-18 23:36:51 -0500

    Initial Set up w/ Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { ListenComponent } from './listen/listen.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'search', component: SearchComponent},
+    {path: 'listen', component: ListenComponent},
+    {path: 'profile', component: ProfileComponent},
+
+    {path: '**', redirectTo:''},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
